6b2a64ecae91059e98d5ea0b83a0fb24effce583
Per Orest <per.orest@outlook.com>
2020-06-30 18:42:40 +0200

    Fixed the login page ;)

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace PRONBS
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsProduction())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
